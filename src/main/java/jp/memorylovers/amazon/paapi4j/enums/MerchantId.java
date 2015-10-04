02cd86a69c446ae5dfc985deb3be4beba564ad3e
@scarlet <memory.loverz@gmail.com>
2015-10-04 14:55:56 +0900

    add MerchantId constant value

---
+++ src/main/java/jp/memorylovers/amazon/paapi4j/enums/MerchantId.java
@@ -0,0 +1,6 @@
+package jp.memorylovers.amazon.paapi4j.enums;
+
+public class MerchantId {
+    public static final String MERCHANT_ALL = "All";
+    public static final String MERCHANT_AMAZON = "Amazon";
+}
