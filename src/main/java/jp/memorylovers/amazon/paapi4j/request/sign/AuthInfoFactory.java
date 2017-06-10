e2c70b0aac4c84d69086d6c098097b17899d24a3
@violet <memory.loverz@gmail.com>
2017-06-10 12:04:53 +0900

    minor change; use dummy value for associate_tag if key is not exist or empty

--- src/main/java/jp/memorylovers/amazon/paapi4j/request/sign/AuthInfoFactory.java
+++ src/main/java/jp/memorylovers/amazon/paapi4j/request/sign/AuthInfoFactory.java
@@ -13,6 +13,7 @@ public class AuthInfoFactory {
     private static final String KEY_ASSOCIATE_TAG = "associate_tag";
     private static final String KEY_ACCESS_KEY = "access_key";
     private static final String KEY_SECRET_KEY = "secret_key";
+    private static final String DUMMY_ASSOCIATE_TAG = "dummy";
 
     /**
      * create AuthInfo class from ${RESOURCE_NAME}.properties file.
@@ -24,13 +25,19 @@ public class AuthInfoFactory {
         ResourceBundle rb = ResourceBundle.getBundle(RESOURCE_NAME);
 
         // check
-        if (!(rb.containsKey(KEY_ASSOCIATE_TAG) && rb.containsKey(KEY_ACCESS_KEY) && rb.containsKey(KEY_ACCESS_KEY))) {
-            String msg = "There is no " + KEY_ASSOCIATE_TAG + " and/or " + KEY_ACCESS_KEY + " and/or " + KEY_SECRET_KEY + " property values " + "in " + RESOURCE_NAME + ".properties";
+        if (!rb.containsKey(KEY_ACCESS_KEY) || !rb.containsKey(
+            KEY_SECRET_KEY)) {
+            String msg = "There is no " + KEY_ACCESS_KEY + " and/or " + KEY_SECRET_KEY + " property values " + "in " + RESOURCE_NAME + ".properties";
             throw new PAAPI4jException(msg);
         }
 
         AuthInfo info = new AuthInfo();
-        info.setAssociateTag(rb.getString(KEY_ASSOCIATE_TAG));
+
+        if (rb.containsKey(KEY_ASSOCIATE_TAG) && !rb.getString(KEY_ASSOCIATE_TAG).isEmpty()) {
+            info.setAssociateTag(rb.getString(KEY_ASSOCIATE_TAG));
+        } else {
+            info.setAssociateTag(DUMMY_ASSOCIATE_TAG);
+        }
         info.setAccessKey(rb.getString(KEY_ACCESS_KEY));
         info.setSecretKey(rb.getString(KEY_SECRET_KEY));
         return info;
